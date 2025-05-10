af4fc827227f5f1facd1ef1a31e63115ec934dae
garvesh <garveshtiwari1998@gmail.com>
2025-05-11 03:38:44 +0530

    book class written

--- entity/Book.java
+++ entity/Book.java
@@ -1,5 +1,48 @@
 package entity;
 
 public class Book {
+
+    private String bookId;
+    private String title;
+    private String author;
+    private boolean isIssued;
+    private double rating;
+
+    public Book(String bookId, String title, String author, double rating) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.rating = rating;
+        this.isIssued = false;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public boolean isIssued() {
+        return isIssued;
+    }
+
+    public void setIssued(boolean issued) {
+        this.isIssued = issued;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + bookId + "] " + title + " by " + author + " rated " + rating + (isIssued ? " (Issued)" : " (Available)");
+    }
     
 }
